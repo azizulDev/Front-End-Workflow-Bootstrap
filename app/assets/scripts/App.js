6bde31504039dd080f462cd20c8a2abf3e84ea09
azizulDev <azizulraju01@gmail.com>
2019-03-30 09:28:27 +0600

    initia release

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,14 @@
+// include jQuery
 let $ = require('jquery');
+
+// include Bootstrap js
 import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
+
+
+
+// custom js ..
 import Bio from './modules/Bio';
 
-let bio = new Bio('Raju', 23);
+let bio = new Bio('Azizul', 23);
 bio.aboutMe();
