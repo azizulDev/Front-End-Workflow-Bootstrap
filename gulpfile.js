7612403a8b9c11a13652ddfad5e0ff0aee164c58
azizulDev <azizulraju01@gmail.com>
2019-03-30 06:13:42 +0600

    Sass to Css task added in watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp'),
 watch = require('gulp-watch'),
+sass = require('gulp-sass'),
 browserSync = require('browser-sync').create()
 
 
@@ -7,6 +8,19 @@ browserSync = require('browser-sync').create()
 gulp.task('html', () => {
   browserSync.reload()
 })
+
+// Sass to Css task
+gulp.task('sassToCss', () => {
+  return gulp.src('./app/assets/sass/custom/style.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./app/temp/css'))
+})
+
+// Css inject task
+gulp.task('cssInject', gulp.series('sassToCss', () => {
+  return gulp.src('./app/temp/css/style.css')
+  .pipe(browserSync.stream())
+}))
 
 
 
@@ -21,4 +35,5 @@ gulp.task('watch', () => {
   })
 
   watch('./app/index.html').on('change', gulp.series('html'))
+  watch('./app/assets/sass/custom/**/*.scss').on('change', gulp.series('cssInject'))
 })
