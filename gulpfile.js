55495724dd423458448ee30e0ff172fea46d44d3
azizulDev <azizulraju01@gmail.com>
2019-03-30 08:24:47 +0600

    build task added.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ watch = require('gulp-watch'),
 sass = require('gulp-sass'),
 autoPrefixer = require('gulp-autoprefixer'),
 webpack = require('webpack'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
 browserSync = require('browser-sync').create()
 
 
@@ -75,3 +81,47 @@ gulp.task('watch', () => {
   watch('./app/assets/sass/bootstrap/**/*.scss').on('change', gulp.series('bootstrapCssInject'))
   watch('./app/assets/scripts/**/*.js').on('change', gulp.series('scriptsRefresh'))
 })
+
+
+// Delete dist folder
+gulp.task('deleteDistFolder', () => {
+  return del('./dist')
+})
+
+
+// Image optimization task
+gulp.task('optimizeImages', gulp.series('deleteDistFolder', () => {
+  return gulp.src('./app/assets/img/**/*')
+  .pipe(imagemin({
+    progressive: true,
+    interlaced: true,
+    multipass: true
+  }))
+  .pipe(gulp.dest('./dist/assets/img'))
+}))
+
+
+// Usemin task
+gulp.task('usemin', gulp.series('deleteDistFolder', 'sassToCss', 'bootstrapSassToCss', () => {
+  return gulp.src('./app/index.html')
+  .pipe(usemin({
+    css: [() => {return rev()}, () => {return cssnano()}, () => {return autoPrefixer()}],
+    js: [() => {return rev()}, () => {return uglify()}]
+  }))
+  .pipe(gulp.dest('./dist'))
+}))
+
+
+// Preview Dist Folder
+gulp.task('previewDist', () => {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'dist'
+    }
+  })
+})
+
+
+// Build task
+gulp.task('build', gulp.series('deleteDistFolder', 'optimizeImages','usemin'))
